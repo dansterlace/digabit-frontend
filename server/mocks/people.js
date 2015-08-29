fd408284b1a5eea6c90b1cac730e955321724ad6
Derek Bassett <pabassett@gmail.com>
2015-08-29 01:02:24 -0500

    People service stand up

---
+++ server/mocks/people.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var express = require('express');
+  var peopleRouter = express.Router();
+
+  peopleRouter.get('/', function(req, res) {
+    res.send({
+      'people': []
+    });
+  });
+
+  peopleRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  peopleRouter.get('/:id', function(req, res) {
+    res.send({
+      'people': {
+        id: req.params.id
+      }
+    });
+  });
+
+  peopleRouter.put('/:id', function(req, res) {
+    res.send({
+      'people': {
+        id: req.params.id
+      }
+    });
+  });
+
+  peopleRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/people', peopleRouter);
+};
