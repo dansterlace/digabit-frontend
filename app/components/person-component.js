b29af85d652cc8a047bcd4b1156d165a17fb2f55
Derek Bassett <pabassett@gmail.com>
2015-08-31 17:26:01 -0500

    Work on Selected Person

--- app/components/person-component.js
+++ app/components/person-component.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	isSelected: Ember.computed('person',function(key, value) {
-		var map = this.get('map'),
-			person = this.get('person');
+	isSelected: undefined
+	// isSelected: Ember.computed('person',function(key, value) {
+	// 	var map = this.get('map'),
+	// 		person = this.get('person');
 
-		// Get
-		if(value === undefined) {
-			return map[person];
-		} else {
-			map[person] = value;
-			return value;
-		}
-	})
+	// 	// Get
+	// 	// if(value === undefined) {
+	// 	// 	return map[person];
+	// 	// } else {
+	// 	// 	map[person] = value;
+	// 	// 	return value;
+	// 	// }
+	// })
 });
--- tests/integration/components/person-component-test.js
+++ tests/integration/components/person-component-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -11,9 +12,11 @@ test('it renders', function(assert) {
   var person = {
     fullName: 'FULL NAME'
   };
+  var map = Ember.Map.create();
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   this.set('person',person);
+  this.set('map', map);
   this.render(hbs`{{person-component}}`);
 
   assert.equal(this.$().text().trim(), '');
