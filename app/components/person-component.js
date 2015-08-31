cbaae86c02fd9cac5c2d2055b41c0da3dcb31f00
Derek Bassett <pabassett@gmail.com>
2015-08-31 17:01:16 -0500

    Persons controller

--- app/components/person-component.js
+++ app/components/person-component.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	isSelected: undefined
+	isSelected: Ember.computed('person',function(key, value) {
+		var map = this.get('map'),
+			person = this.get('person');
+
+		// Get
+		if(value === undefined) {
+			return map[person];
+		} else {
+			map[person] = value;
+			return value;
+		}
+	})
 });
